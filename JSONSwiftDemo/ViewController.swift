c8ea7eb5c7c183bf37f1e852edba042802c282f4
Nietzsky <Nietzsky@hive.fritz.box>
2017-09-02 00:00:46 +0200

    Initial Commit

---
+++ JSONSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONSwiftDemo
+//
+//  Created by Nietzsky on 02/09/2017.
+//  Copyright © 2017 Testerik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
